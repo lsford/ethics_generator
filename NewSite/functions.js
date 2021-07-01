cf8d3c9a829181cb9c3905531fc6d8b49619f25c
lsford <79148554+lsford@users.noreply.github.com>
2021-07-01 22:26:25 +0100

    Updated scenarios, added javascript to cdss page, updated cdss page

--- NewSite/functions.js
+++ NewSite/functions.js
@@ -1,16 +1,4 @@
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("text");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 8000); // Change image every 2 seconds
+function startScenarios() {
+  document.getElementById("title-cdss").innerHTML =
+    "Healthcare Information Technology Scenarios";
 }
