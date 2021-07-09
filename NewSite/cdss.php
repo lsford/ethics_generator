861f3bb4aea1b122908a9ba7f02fe6cd1a8788d1
lsford <79148554+lsford@users.noreply.github.com>
2021-07-09 18:36:45 +0100

    Updated scenarios

---
+++ NewSite/cdss.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>About the Ethics Generator</title>
+    <link rel="icon" href="/2890562-artificialintelligence11_101977.ico" />
+    <meta name="viewport" content="width=device.width, initial-scale=1" />
+    <link rel="icon" href="/img/2890562-artificialintelligence11_101977.ico" />
+    <link rel="stylesheet" href="/NewSite/style.css" />   
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    <body id="scenario-body">
+
+        <header>
+            <nav class="navigation-index">
+               <a href="/NewSite/index.html"><h1 id="title">ƎE Exploring Ethics</h1></a>
+              <!--<img src="Logo1.png" height="50" width="">-->
+               <ul>
+                <li><a href="/NewSite/index.html">Home</a></li>
+          <li><a href="/NewSite/scenarios.html">Scenarios</a></li>
+          <li><a href="/NewSite/about.html">About</a></li>
+              </ul>
+              <div class="clear"></div>
+            </nav>
+          </header>
+<body>
+	
+</body>
+</html>
