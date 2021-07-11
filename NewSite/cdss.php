4c233c95578389c2df5e179127c17cacefbac3a9
lsford <79148554+lsford@users.noreply.github.com>
2021-07-11 19:05:50 +0100

    Update cdss.php

--- NewSite/cdss.php
+++ NewSite/cdss.php
@@ -5,10 +5,11 @@
     <link rel="icon" href="/2890562-artificialintelligence11_101977.ico" />
     <meta name="viewport" content="width=device.width, initial-scale=1" />
     <link rel="icon" href="/img/2890562-artificialintelligence11_101977.ico" />
-    <link rel="stylesheet" href="/NewSite/style.css" />   
+    <link rel="stylesheet" href="style.css" />   
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+<script src="functions.js"></script>
     <body id="scenario-body">
 
         <header>
@@ -23,6 +24,26 @@
               <div class="clear"></div>
             </nav>
           </header>
+
 </head>
+<main>
+<h3 id="purpose">Healthcare Information Technology</h3>
+ 
+    <div>
+      <p id="description"> Here you will be presented with a series of scenarios about the use of artificial intelligence in the healthcare industry. Each scenario will include a visual and written description of a situation, and in some cases the situations outcome. </p>
+      <p id="description">For each scenario please select the situation you find preferable. There is an example scenario below, please press <b>start</b> when you are ready to begin. No knowledge of the topic is required to answer the questions.</p> 
+    </div>
+    
+    <div id="line">
+      <h3 id="title-cdss">Example Scenario</h3>
+    </div>
+       
+      <div id="cdss-container">
+      <p>Example scenario where you have to choose one of two options decided by an artificial intelligence. </p>
+        <div>
+          <button id="start-button" onclick="startScenarios()"> Start Judging</button>
+        </div>
+      </div>
+</main>
 	</body>
 </html>
